c9bf857c022ab15c94d432ddd0d10b3ae753a41c
sebas524 <104307242+sebas524@users.noreply.github.com>
2023-10-12 11:46:44 +0200

    fixed html entities problem

--- src/app/quiz/services/api.service.ts
+++ src/app/quiz/services/api.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, map } from 'rxjs';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { environments } from 'src/app/environments/environments';
 import { OpenTriviaDB, Result } from '../interfaces/openTriviaDb.interface';
 import { Question } from '../interfaces/question.interface';
@@ -13,6 +14,16 @@ export class ApiService {
 
   constructor(private http: HttpClient) {}
 
+  // ! decodeHtmlEntities is just because api shows "" incorrectly!!
+  private decodeHtmlEntities(input: string | null): string {
+    if (input === null) {
+      return ''; // Or handle the null value in some appropriate way
+    }
+
+    const doc = new DOMParser().parseFromString(input, 'text/html');
+    return doc.documentElement.textContent || ''; // Handle potential null value
+  }
+
   fetchApiQuestions(): Observable<Question[]> {
     return this.http
       .get<OpenTriviaDB>(
@@ -22,15 +33,15 @@ export class ApiService {
         map((data) => {
           return data.results.map((result: Result) => {
             const question: Question = {
-              description: result.question,
+              description: this.decodeHtmlEntities(result.question), // Decode HTML entities
               answers: [
                 {
-                  name: result.correct_answer,
-                  isRight: 1, // Set isRight to 1 for the correct answer
+                  name: this.decodeHtmlEntities(result.correct_answer), // Decode HTML entities for correct answer
+                  isRight: 1, //* Set isRight to 0 for correct answers
                 },
                 ...result.incorrect_answers.map((incorrectAnswer) => ({
-                  name: incorrectAnswer,
-                  isRight: 0, // Set isRight to 0 for incorrect answers
+                  name: this.decodeHtmlEntities(incorrectAnswer), // Decode HTML entities
+                  isRight: 0, //* Set isRight to 0 for incorrect answers
                 })),
               ],
             };
